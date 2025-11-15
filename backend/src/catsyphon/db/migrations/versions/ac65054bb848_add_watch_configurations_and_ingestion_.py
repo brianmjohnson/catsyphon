3a6400eefe83b89203483b73dfc826165d6529fc
Kulesh Shanmugasundaram <164083+kulesh@users.noreply.github.com>
2025-11-14 20:45:20 -0500

    style: fix import order in migration file

--- backend/src/catsyphon/db/migrations/versions/ac65054bb848_add_watch_configurations_and_ingestion_.py
+++ backend/src/catsyphon/db/migrations/versions/ac65054bb848_add_watch_configurations_and_ingestion_.py
@@ -8,8 +8,8 @@ Create Date: 2025-11-14 16:05:34.452785
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
